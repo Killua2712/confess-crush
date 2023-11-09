f92e924481867f55cbd95485165193dbbe038239
Killua2712 <150322067+Killua2712@users.noreply.github.com>
2023-11-09 13:20:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Dear my Crush",
+    introTitle: 'starts with the letter T',
+    introDesc: `Anh rất hạnh phúc vì được gặp em lần đầu tiên ở TS, em là điều tuyệt vời nhất trong cuộc sống của anh`,
+    btnIntro: 'Love u',
+    title: 'Say men rượu qua đêm sẽ tỉnh ',
+    desc: 'Say men tình mãi vẫn còn say  ',
+    btnYes: 'Đúng không nà',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao mỗi lần đi nhậu a đều có may mắn được gặp e <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Bất cứ khi nào e say, hãy alo cho a vì a là men tình của e .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
